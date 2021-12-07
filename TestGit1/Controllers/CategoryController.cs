62dbfc936813268e74607baaedd716c46e74b2a4
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2021-12-07 12:56:19 +0700

    add category

---
+++ TestGit1/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGit1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public IActionResult GetCategory()
+        {
+            return Ok(new { V = 1,c= 2,d = 3 });
+        }
+    }
+}
