fced53dd62cd2bd3173429bcfe407e23731453c3
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2021-12-07 08:59:48 +0700

    add product controller

---
+++ TestGit1/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGit1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("GetJson")]
+        public IActionResult GetJson()
+        {
+            int[] a = new int[3] { 1, 2, 3 };
+            object ob = a;
+            return Ok(ob);
+        }
+    }
+}
