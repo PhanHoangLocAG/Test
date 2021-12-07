54a0285558dd86173146629a38a3e767e3728fd6
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2021-12-07 13:11:20 +0700

    invoice model

---
+++ TestGit1/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGit1.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
